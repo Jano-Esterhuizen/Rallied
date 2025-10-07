fb16cfc3c72dc3be9150330b1b045388a95fd1e5
Jano Personal <esterhuizenjano@gmail.com>
2025-10-07 10:57:39 +0200

    Created Navbar

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,7 @@
-import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import { Container, CssBaseline, List, ListItem, ListItemText } from "@mui/material";
 import axios from "axios";
 import {useEffect, useState } from "react"
+import NavBar from "./NavBar";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -15,14 +16,22 @@ function App() {
 
   return (
     <>
-      <Typography variant='h3'>Rallied</Typography>
-      <List>
+    
+    <CssBaseline/> 
+    {/* Used to remove padding around the NavBar, Normalize styles across browsers
+    Removes inconsistencies in things like default margins, font sizes, and line heights. */}
+      <NavBar/>
+      <Container maxWidth='xl' sx={{mt: 3}}>
+        <List>
         {activities.map((activity) => (
           <ListItem key={activity.id}>
             <ListItemText>{activity.title}</ListItemText>
           </ListItem>
         ))}
       </List>
+
+      </Container>
+      
     </>
 
   )
